49fc2b3d70c9c4e0567d558ea328595b3dba662b
patrickwalter1952 <patrickwalter1952@gmail.com>
2023-11-12 19:51:52 -0600

    added sum & avg

--- src/HelloThere.py
+++ src/HelloThere.py
@@ -4,3 +4,12 @@ Created on Nov 12, 2023
 @author: walte
 '''
 print("Hello There")
+
+nbrs = {3, 5, 6, 8, 12, 14}
+
+sum = 0
+for i in nbrs :
+    sum += i
+
+avg = sum / len(nbrs)
+print(f"sum {sum}  average: {avg}")
